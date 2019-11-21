8350976e4bbe5f48be2a0b008187fa7abe8ae541
ferag <ferag@live.com>
2019-11-21 10:01:05 +0000

    Access token workflow implemented

--- keystone_oidc_auth_plugin/auth.py
+++ keystone_oidc_auth_plugin/auth.py
@@ -22,10 +22,17 @@ from keystone.auth.plugins import mapped as ks_mapped
 import keystone.conf
 from keystone import exception
 from keystone.i18n import _
+from keystone.server.flask import common as ks_flask
+from keystone.server.flask.request_processing import json_body
+from keystone.server.flask.request_processing import req_logging
+
 import oic.exception
 from oic import oic
+from oic.oic.message import AuthorizationResponse
 from oic.utils.authn import client as utils_client
+from oic.utils.authn.client import CLIENT_AUTHN_METHOD
 from oic.utils import jwt
+
 from oslo_config import cfg
 from oslo_log import log
 
@@ -47,6 +54,22 @@ opts = [
     cfg.StrOpt(
         "client_id",
         help="Client identifier used in calls to the OpenID Connect Provider"),
+    cfg.StrOpt(
+        "authorization_endpoint",
+        help="OpenID connect issuer URL. We will use this to build all the "
+             "in Keystone."),
+    cfg.StrOpt(
+        "client_secret",
+        help="Client identifier only known by the application and Identity provider client"),
+    cfg.StrOpt(
+        "scope",
+        help="Supported OpenID scopes in the Identity provider"),
+    cfg.StrOpt(
+        "token_endpoint",
+        help="OpenID connect URL to get identity and access tokens"),
+    cfg.StrOpt(
+        "redirect_uri",
+        help="Application (keystone) URL to post Identity provider and user information"),
 ]
 
 global_opts = [
@@ -72,7 +95,6 @@ CONF.register_opts(global_opts, group="openid")
 
 class InvalidOauthToken(exception.ValidationError):
     message_format = _('No valid OAuth 2.0 token has been found in headers.')
-
 
 class OpenIDConnect(ks_mapped.Mapped):
     """Provide OpenID Connect authentication.
@@ -130,7 +152,8 @@ class OpenIDConnect(ks_mapped.Mapped):
 
     def authenticate(self, auth_payload):
         assertion = ks_mapped.extract_assertion_data()
-
+        LOG.debug("aguilarf assertion: %s" % assertion)
+        LOG.debug("aguilarf request: %s" % flask.request.environ['QUERY_STRING'])
         # Handle Bearer auth, this is not "pure" OpenID Connect but it is
         # required to work with the current keystoneauth1 code, that allows
         # users to register the OpenStack CLI as an OpenID Connect client,
@@ -149,10 +172,17 @@ class OpenIDConnect(ks_mapped.Mapped):
                 raise InvalidOauthToken()
 
             self.handle_bearer(auth_payload, access_token)
+            return super(OpenIDConnect, self).authenticate(auth_payload)       
+        # Get a new token based on config
         else:
-            pass
-
-        return super(OpenIDConnect, self).authenticate(auth_payload)
+            if 'QUERY_STRING' in flask.request.environ and 'code' in flask.request.environ['QUERY_STRING']:
+                access_token = self.get_access_token(auth_payload,assertion)
+                self.handle_bearer(auth_payload, access_token)
+                return super(OpenIDConnect, self).authenticate(auth_payload)
+            else:
+                LOG.debug("Bearer token was not received, getting OAuth token")
+                login_url = self.handle_access_token(auth_payload)
+                raise exception.RedirectRequired(login_url)
 
     def handle_bearer(self, auth_payload, access_token):
         identity_provider = self._get_idp_from_payload(auth_payload)
@@ -173,6 +203,56 @@ class OpenIDConnect(ks_mapped.Mapped):
         # consumed downstream, and we are done
         set_env_params_from_dict(claims)
 
+    def handle_access_token(self, auth_payload):
+        client = oic.Client(client_authn_method=utils_client.CLIENT_AUTHN_METHOD, verify_ssl=False)
+        identity_provider = self._get_idp_from_payload(auth_payload)
+        conf = configuration.Configuration(opts,
+                                           "openid_%s" % identity_provider)
+        provider_info = client.provider_config(conf.issuer)
+        session = {"nonce": oic.rndstr(), "state": oic.rndstr()}
+        args = {
+               "response_type": 'code',
+               "client_id": conf.client_id,
+               "authorization_endpoint": conf.authorization_endpoint,
+               "client_secret": conf.client_secret,
+               "token_endpoint": conf.token_endpoint,
+               "redirect_uri": conf.redirect_uri,
+               "scope": provider_info["scopes_supported"],
+               "nonce": session["nonce"],
+               "state": session["state"],
+            }
+        auth_req = client.construct_AuthorizationRequest(request_args=args)
+        login_url = auth_req.request(client.authorization_endpoint)
+
+        return login_url
+
+    def get_access_token(self, auth_payload,assertion):
+        identity_provider = self._get_idp_from_payload(auth_payload)
+        oidc_client = self.get_oidc_client(identity_provider)
+
+        LOG.debug("Lets assert response")
+        response = flask.request.environ["QUERY_STRING"]
+
+        conf = configuration.Configuration(opts,
+                                           "openid_%s" % identity_provider)
+        LOG.debug('Create aresp')
+        aresp = oidc_client.parse_response(AuthorizationResponse, info=response, sformat="urlencoded")
+        code = aresp["code"]
+        args = {
+               "code": code,
+               "client_id": conf.client_id,
+               "authorization_endpoint": conf.authorization_endpoint,
+               "client_secret": conf.client_secret,
+               "token_endpoint": conf.token_endpoint,
+               "redirect_uri": conf.redirect_uri,
+               "scope": conf.scope
+            }
+        oidc_client.client_id=conf.client_id
+        oidc_client.authorization_endpoint = conf.authorization_endpoint
+        resp = oidc_client.do_access_token_request(state=aresp["state"],request_args=args,authn_method="client_secret_basic")
+        #TODO this is a test for assertion
+        access_token = resp['access_token'] #Importante! para mapeo
+        return access_token
 
 def set_env_params_from_dict(d):
     prefix = CONF.openid.claim_prefix
