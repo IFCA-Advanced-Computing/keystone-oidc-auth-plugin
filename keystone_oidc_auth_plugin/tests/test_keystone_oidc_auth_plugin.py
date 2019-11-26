5d8720a41be0b1c87b8b862eeb4cdca3df47a584
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2019-11-26 12:57:27 +0100

    Fix unit test

--- keystone_oidc_auth_plugin/tests/test_keystone_oidc_auth_plugin.py
+++ keystone_oidc_auth_plugin/tests/test_keystone_oidc_auth_plugin.py
@@ -19,16 +19,42 @@ test_keystone_oidc_auth_plugin
 Tests for `keystone_oidc_auth_plugin` module.
 """
 
+import mock
+import uuid
 
+from keystone.api._shared import authentication
+from keystone import auth
 from keystone.tests.unit.ksfixtures import auth_plugins
 from keystone.tests.unit import test_auth_plugin as ks_test_auth_plugin
+
+from keystone_oidc_auth_plugin import auth as auth_plugin
 
 
 class TestKeystone_oidc_auth_plugin(ks_test_auth_plugin.TestMapped):
 
     def test_load_openid_ifca(self):
-        self.useFixture(auth_plugins.ConfigAuthPlugins(self.config_fixture,
-                                                       ["openid"],
-                                                       openid="ifca"))
-        self.useFixture(auth_plugins.LoadAuthPlugins("openid"))
-        self._test_mapped_invocation_with_method_name("openid")
+        method_name = "openid"
+
+        with mock.patch.object(auth_plugin.OpenIDConnect,
+                               'authenticate',
+                               return_value=None) as authenticate:
+
+            self.useFixture(auth_plugins.ConfigAuthPlugins(self.config_fixture,
+                                                           [method_name],
+                                                           openid="ifca"))
+            self.useFixture(auth_plugins.LoadAuthPlugins(method_name))
+            auth_data = {
+                'identity': {
+                    'methods': [method_name],
+                    method_name: {'protocol': method_name},
+                }
+            }
+            auth_info = auth.core.AuthInfo.create(auth_data)
+            auth_context = auth.core.AuthContext(
+                method_names=[],
+                user_id=uuid.uuid4().hex)
+            with self.make_request():
+                authentication.authenticate(auth_info, auth_context)
+            # make sure Mapped plugin got invoked with the correct payload
+            ((auth_payload,), kwargs) = authenticate.call_args
+            self.assertEqual(method_name, auth_payload['protocol'])
